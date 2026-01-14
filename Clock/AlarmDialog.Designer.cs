3777d6de73ffae922c725e41f3ff79e2cb5696b8
Danila <Danila@DESKTOP-44LP0PV>
2026-01-14 20:31:42 +0300

    TODO: alarm Dialog Weekdays

--- Clock/AlarmDialog.Designer.cs
+++ Clock/AlarmDialog.Designer.cs
@@ -100,6 +100,7 @@
 			this.clbWeekdays.Name = "clbWeekdays";
 			this.clbWeekdays.Size = new System.Drawing.Size(350, 30);
 			this.clbWeekdays.TabIndex = 4;
+			this.clbWeekdays.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.clbWeekdays_ItemCheck);
 			// 
 			// labelFileName
 			// 
